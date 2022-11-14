9c64135cf738b9b41e322d3ff3bba4f94ea04157
Harshad0018 <harshadpawar0018@gmail.com>
2022-11-14 15:39:44 +0530

    This is First commit

---
+++ Jenkin_Spring-1/src/main/java/JenkinPkg/Jenkin.java
@@ -0,0 +1,8 @@
+package JenkinPkg;
+
+public class Jenkin {
+	public static void main(String[] args) {
+		System.out.println("This is Maven Project");
+
+	}
+}
---
+++ Jenkin_Spring-1/src/main/java/com/project/demo/JenkinSpring1Application.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinSpring1Application.class, args);
+	}
+
+}
